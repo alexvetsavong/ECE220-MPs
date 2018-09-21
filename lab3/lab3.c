72491b878b2282efd8a4c88a3470dce152582008
Alex Vetsavong <avetsa2@illinois.edu>
2018-09-21 11:26:21 -0500

    Completed Lab 3

--- lab3/lab3.c
+++ lab3/lab3.c
@@ -14,17 +14,21 @@
 int main()
 {
     // Declare variables
-
+    int i, n;
+    float w1, w2, x, result;
     // Prompt user for input
-
+    printf("Please enter n, w1, w2: ");
     // Get user input
-
+    scanf("%d %f %f", &n, &w1, &w2);
     // Compute function output
     /* for i from 0 to n-1
      *     compute and print xi and f(xi)
      *     use sin() function from math.h
      */
-
+    for(i=0;i<n-1;i++){
+      x = i*M_PI/n;
+      result = sin(w1*x) + 0.5*sin(w2*x);
+      printf("Result: (%f,%f)\n", x, result);
+    }
     return 0;
 }
-
