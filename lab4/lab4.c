e51952336cb538d068213aad564038ac496524c8
Alex Vetsavong <avetsa2@illinois.edu>
2018-09-28 11:46:57 -0500

    finished lab4.c and prime.c code

--- lab4/lab4.c
+++ lab4/lab4.c
@@ -2,9 +2,16 @@
 #include "prime.h"
 
 int main(){
+int i, n;
+  printf("Enter the value for n: ");
+  scanf("%d", &n);
+  printf("Printing primes less than or equal to %d\n", n);
   // Write the code to take a number n from user and print all the prime numbers between 1 and n
-
-
+  for (i=1; i <= n; i++){
+    if (is_prime(i) != 0){
+      printf("%d ", i);
+    }
+  }
+  printf("\n");
   return 0;
 }
-
--- lab4/prime.c
+++ lab4/prime.c
@@ -2,6 +2,11 @@
 
 int is_prime(int n){
   // Return 1 if n is a prime, and 0 if it is not
-  return 0;
+  int i;
+  for (i=2; i < n-1; i++)
+  if (n%i == 0)
+  {
+    return 0;
+  }
+  return 1;
 }
-
