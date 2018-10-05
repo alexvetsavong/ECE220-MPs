295b585d4089de8b6b95e9c0f2020bb523415594
avetsa2 <avetsa2@illinois.edu>
2018-10-05 12:46:51 -0500

    move lab5 to repo for testings on EWS

--- lab5/dice.c
+++ lab5/dice.c
@@ -1,5 +1,10 @@
 //Function for generating three d6 rolls
+#include <stdlib.h>
+
+int rand(){}
 
 void roll_three(int* one, int* two, int* three){
-
+	*one = rand() % 6;
+	*two = rand() % 6;
+	*three = rand() % 6;
 }
--- lab5/main.c
+++ lab5/main.c
@@ -8,4 +8,22 @@
  * Otherwise print "\n"
  */
 
+#include <stdio.h>
+#include <stdlib.h>
+#include "dice.h"
 
+void srand(unsigned int seed){}
+
+int main(){
+	unsigned int seed, one, two, three;
+	printf("Enter a seed: \n");
+	scanf("%u", &seed);
+	srand((unsigned) seed);
+	roll_three(&one, &two, &three);
+	printf("%u %u %u ", one, two, three);
+	if (one == two == three){
+		printf("Triple!\n");
+	}
+	else if (one == two || one == three || two == three) printf("Double!\n");
+	return 0;
+}	
