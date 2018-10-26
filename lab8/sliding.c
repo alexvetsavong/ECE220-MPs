754751b31aa9a85c71e67d32446c4cebbb19bd8b
avetsa2 <avetsa2@illinois.edu>
2018-10-26 12:11:52 -0500

    Completed lab8 for slide_up in sliding.c

--- lab8/sliding.c
+++ lab8/sliding.c
@@ -2,6 +2,18 @@
 /*  Slide all values of array up
 */
 void slide_up(int* my_array, int rows, int cols){
-
+  int i, j, k; // iterator variables
+  for(j=0; j < cols; j++){
+    for(i=1; i < rows; i++){
+      if(my_array[(i)*cols+j] > 0){
+        for(k=0; k < i; k++){
+          if (my_array[(k)*cols+j] < 0){
+            my_array[(k)*cols+j] = my_array[(i)*cols+j];
+            my_array[(i)*cols+j] = -1;
+          }
+        }
+      }
+    }
+  }
     return;
 }
